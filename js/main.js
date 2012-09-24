ebcc6ccf985323d742345ede140f8c9021cb446c
Julian Jyu <belleberstinge@gmail.com>
2012-09-24 12:15:22 +0800

    I'm a jQuery ninjja

--- js/main.js
+++ js/main.js
@@ -2,6 +2,18 @@
 // $("img").width(200);
 $(document).ready(function(){
 	// code here will run when the HTML document is loaded.
+	$("li").on("click", function(){
+
+		// refers to own object
+		if ($(this).hasClass("done") {
+			$(this).removeClass("done");
+		} else {
+			$(this).addClass("done");
+		}
+
+		// refers to arrays of all "li"s
+		// $("li").addClass("done");
+	});
 
 	$("img").on("dblclick", function(){
 		$("img").width(200);
